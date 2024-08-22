bd30bfebd64e3435dc767937007f745bd07c0cfb
JavierAnahuac <javier.serrano@anahuac.mx>
2024-08-21 21:52:11 -0600

    Clase de prefab

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform spawnPoint;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject newBullet = Instantiate(bulletPrefab);
+            newBullet.transform.position = spawnPoint.position;
+            newBullet.transform.rotation = spawnPoint.rotation;
+            newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.transform.forward * 100.0f);
+        }
+    }
+}
