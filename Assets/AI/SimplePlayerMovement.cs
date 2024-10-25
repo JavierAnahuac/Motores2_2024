77b991143b30ef898867d6182351b321126d8ad5
JavierAnahuac <javier.serrano@anahuac.mx>
2024-10-24 21:11:50 -0600

    timeline

--- Assets/AI/SimplePlayerMovement.cs
+++ Assets/AI/SimplePlayerMovement.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class SimplePlayerMovement : MonoBehaviour
 {
     public float speed = 1.0f;
+    public bool canMove = true;
 
-    // Update is called once per frame
     void Update()
     {
+        if (!canMove) return;
         Vector3 pos = this.transform.position;
         float x = pos.x += (Input.GetAxis("Horizontal") * speed * Time.deltaTime);
         float z = pos.z += (Input.GetAxis("Vertical") * speed * Time.deltaTime);
         this.transform.position = new Vector3(x, pos.y, z);
+    }
+
+    public void SetCanMoveFalse()
+    {
+        canMove = false; 
+    }
+
+    public void SetCanMoveTrue()
+    {
+        canMove = true;
     }
 }
